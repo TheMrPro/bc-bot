d8164448182f3ed4df69b0e946f4f7fe3049a323
TheMrPro <40274555+TheMrPro@users.noreply.github.com>
2018-12-17 21:31:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -160,7 +160,7 @@ m.sendMessage(args)
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag} !`);
-          client.user.setActivity("v!help | v!invite",{type: 'WATCHING'});
+          client.user.setActivity("#$uicide s!help | s!invite",{type: 'WATCHING'});
   
   });
 
